9c68197f2ebf9be022e6b927d294588aa101ff9f
Raul Bras <139993852+teknolista@users.noreply.github.com>
2025-07-18 11:06:46 -0300

    Added better comments to explain the motives.

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,31 @@
+/*
+ * You need to add the following setting to your VS Code settings 
+ * so that characters like '@>' have the correct colorization:
+ * 
+ *     "editor.bracketPairColorization.enabled": false
+ * 
+ * This way, VS Code won't complain if the '>' character comes
+ * right after '@' and doesn't have a '<' before it to match.
+ */
 import vscode = require("vscode");
 
+// code that runs on activation of the extension, 
+// setting validation of brackts false, so magic-statements
+// like '@>' dont have problems with syntax-highlighting.
 export function activate(context: vscode.ExtensionContext) {
-  // code that runs on activation of the extension,
-  // set validation of brackts false, so magic-statements
-  // like @> dont have problems with syntax-highlighting.
-  vscode.workspace.getConfiguration().update(
-    "editor.bracketPairColorization.enabled",
-    false,
-    vscode.ConfigurationTarget.Global
-  );
   vscode.workspace.getConfiguration().update(
     "editor.bracketPairColorization.enabled",
     false,
     vscode.ConfigurationTarget.Workspace
   );
+
+  // Yeah, I know; but just to be sure and have no surprises.
+  vscode.workspace.getConfiguration().update(
+    "editor.bracketPairColorization.enabled",
+    false,
+    vscode.ConfigurationTarget.Global
+  );
+
 }
 
 export function deactivate() { }
